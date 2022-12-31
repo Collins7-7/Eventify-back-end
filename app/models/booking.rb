0ccb25452f456b27719a0441ecded323311f5465
COllins <collinsmuiruri77@gmail.com>
2022-12-31 19:50:13 +0300

    Added controllers and created actions for event, category and booking models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  validates :no_of_tickets, :amount_paid, presence: true
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ApplicationRecord
     has_many :events
+
+    validates :name, presence: true
+
+    validates :name, inclusion: { in: ["Business", "Technology", "Art Workshops", "Sports", "Music Concerts", "Community"] }
     
 end
